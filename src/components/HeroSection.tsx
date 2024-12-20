2669f4382c4c2943863df9d51c2e0bc4d5a53885
Sh-karan27 <shuklakaran107@gmail.com>
2024-12-20 22:19:47 +0530

    Hero section added

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { hero_section_arr } from '../data/hero_section';
+import { ImagesSliderDemo } from './ImagesSliderDemo';
 
-const HeroSection = () => {
-  return <div>HeroSection</div>;
+const HeroSection: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => {
+        const newIndex = (prevIndex + 1) % hero_section_arr.length;
+        console.log('Current Index:', newIndex); // Debugging
+        return newIndex;
+      });
+    }, 3000); // Auto-swipe every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className='hero-section min-h-screen'>
+      <ImagesSliderDemo />
+    </div>
+  );
 };
 
 export default HeroSection;
---
+++ src/data/hero_section.ts
@@ -0,0 +1,43 @@
+// import
+import HeroVector1 from '../assets/HeroVector1.png';
+import HeroVector2 from '../assets/HeroVector2.png';
+
+// Hero Image Array
+
+export const hero_section_arr: { image: string; title: string }[] = [
+  {
+    title: 'one',
+    image:
+      'https://images.pexels.com/photos/2280568/pexels-photo-2280568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    title: 'two',
+    image:
+      'https://images.pexels.com/photos/9629715/pexels-photo-9629715.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    title: 'three',
+    image:
+      'https://images.pexels.com/photos/40568/medical-appointment-doctor-healthcare-40568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    title: 'four',
+    image:
+      'https://images.pexels.com/photos/7581074/pexels-photo-7581074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    title: 'five',
+    image:
+      'https://images.pexels.com/photos/4101143/pexels-photo-4101143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    title: 'six',
+    image:
+      'https://images.pexels.com/photos/7653116/pexels-photo-7653116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
+
+// export
+
+export const HeroVectorOne: string = HeroVector1;
+export const HeroVectorTwo: string = HeroVector2;
