b9fc6387efa3b897878c7a3c5b1f9915e5b18660
Sh-karan27 <shuklakaran107@gmail.com>
2024-12-23 21:26:50 +0530

    Testimonial & Best Doctor section Added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import HeroSection from '../components/HeroSection';
-import Specialist from '../components/Specialist';
-import BestDoctors from '../components/BestDoctors';
-import TestBooking from '../components/TestBooking';
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import Specialist from "../components/Specialist";
+import BestDoctors from "../components/BestDoctors";
+import TestBooking from "../components/TestBooking";
+import { AnimatedTestimonialsDemo } from "../components/AnimatedTestimonials";
 
 const Home = () => {
   return (
@@ -11,8 +12,10 @@ const Home = () => {
       <Specialist />
       <BestDoctors />
       <TestBooking />
+      <AnimatedTestimonialsDemo />
     </>
   );
 };
 
 export default Home;
+
