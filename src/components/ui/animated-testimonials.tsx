eeec2609a30071428dd4008b0b16c7e2c2bcee1d
Sh-karan27 <shuklakaran107@gmail.com>
2024-12-26 15:58:26 +0530

    responsiveness added, Testimonial section fixed

--- src/components/ui/animated-testimonials.tsx
+++ src/components/ui/animated-testimonials.tsx
@@ -1,8 +1,6 @@
-
-
-import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
-import { motion, AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from 'react-icons/fa';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 type Testimonial = {
   quote: string;
@@ -42,10 +40,10 @@ export const AnimatedTestimonials = ({
     return Math.floor(Math.random() * 21) - 10;
   };
   return (
-    <div className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20">
-      <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20">
+    <div className='max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20'>
+      <div className='relative grid grid-cols-1 md:grid-cols-2  gap-20'>
         <div>
-          <div className="relative h-80 w-full">
+          <div className='relative h-80 w-full z-0'>
             <AnimatePresence>
               {testimonials.map((testimonial, index) => (
                 <motion.div
@@ -74,24 +72,23 @@ export const AnimatedTestimonials = ({
                   }}
                   transition={{
                     duration: 0.4,
-                    ease: "easeInOut",
+                    ease: 'easeInOut',
                   }}
-                  className="absolute inset-0 origin-bottom"
-                >
+                  className='absolute inset-0 origin-bottom'>
                   <img
                     src={testimonial.src}
                     alt={testimonial.name}
-                    width={500}
-                    height={500}
+                    width={700}
+                    height={700}
                     draggable={false}
-                    className="h-full w-full rounded-3xl object-cover object-center"
+                    className='h-full w-full rounded-3xl object-cover object-center '
                   />
                 </motion.div>
               ))}
             </AnimatePresence>
           </div>
         </div>
-        <div className="flex justify-between flex-col py-4">
+        <div className='flex justify-between flex-col py-4'>
           <motion.div
             key={active}
             initial={{
@@ -108,53 +105,49 @@ export const AnimatedTestimonials = ({
             }}
             transition={{
               duration: 0.2,
-              ease: "easeInOut",
-            }}
-          >
-            <h3 className="text-2xl font-bold dark:text-white text-black">
+              ease: 'easeInOut',
+            }}>
+            <h3 className='text-2xl font-bold dark:text-white text-black'>
               {testimonials[active].name}
             </h3>
-            <p className="text-sm text-text-light ">
+            <p className='text-sm text-text-light '>
               {testimonials[active].designation}
             </p>
-            <motion.p className="text-lg text-gray-500 mt-8 dark:text-neutral-300">
-              {testimonials[active].quote.split(" ").map((word, index) => (
+            <motion.p className='text-lg text-gray-500 mt-8 dark:text-neutral-300'>
+              {testimonials[active].quote.split(' ').map((word, index) => (
                 <motion.span
                   key={index}
                   initial={{
-                    filter: "blur(10px)",
+                    filter: 'blur(10px)',
                     opacity: 0,
                     y: 5,
                   }}
                   animate={{
-                    filter: "blur(0px)",
+                    filter: 'blur(0px)',
                     opacity: 1,
                     y: 0,
                   }}
                   transition={{
                     duration: 0.2,
-                    ease: "easeInOut",
+                    ease: 'easeInOut',
                     delay: 0.02 * index,
                   }}
-                  className="inline-block"
-                >
+                  className='inline-block'>
                   {word}&nbsp;
                 </motion.span>
               ))}
             </motion.p>
           </motion.div>
-          <div className="flex gap-4 pt-12 md:pt-0">
+          <div className='flex gap-4 pt-12 md:pt-0'>
             <button
               onClick={handlePrev}
-              className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button"
-            >
-              <FaLongArrowAltLeft className="text-black dark:text-neutral-400 group-hover/button:rotate-12 transition-transform duration-300" />
+              className='h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button'>
+              <FaLongArrowAltLeft className='text-black dark:text-neutral-400 group-hover/button:rotate-12 transition-transform duration-300' />
             </button>
             <button
               onClick={handleNext}
-              className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button"
-            >
-              <FaLongArrowAltRight className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:-rotate-12 transition-transform duration-300" />
+              className='h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button'>
+              <FaLongArrowAltRight className='h-5 w-5 text-black dark:text-neutral-400 group-hover/button:-rotate-12 transition-transform duration-300' />
             </button>
           </div>
         </div>
